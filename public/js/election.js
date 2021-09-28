4d04c529e55964e4ed1eb298e975620d20eeadbc
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-09-28 03:19:32 +0000

    Election displayed in table.

--- public/js/election.js
+++ public/js/election.js
@@ -1,5 +1,33 @@
 const URL = "http://localhost:3100";
 
+const displayElections = () => {
+    fetch(`${URL}/election`)
+        .then(response => response.json())
+        .then(data => {
+            let table = document.querySelector("tbody");
+            let election_data = data.reverse();
+
+            table.innerHTML = "";
+            election_data.forEach((ele, index) => {
+                let html = `<tr id=${ele.id}>
+                    <th scope="row">${index + 1}.</th>
+                    <td>${ele.election_name}</td>
+                    <td>
+                        <button class="btn outline btn-outline-primary btn-floating">
+                            <i class="fas fa-pencil-alt"></i>
+                        </button>
+                        <button type="button" class="btn btn-outline-danger btn-floating">
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </td>
+                </tr>`;
+
+                table.insertAdjacentHTML('beforeend', html);
+            });
+        })
+}
+displayElections();
+
 // Create Election Button
 document.querySelector('#create-election-btn').addEventListener('click', (e) => {
     let election_name = document.querySelector("#new-election-name").value;
@@ -15,6 +43,7 @@ document.querySelector('#create-election-btn').addEventListener('click', (e) => 
                 timer: 2500
             })
             document.querySelector("#new-election-name").value = "";
+            displayElections();
         })
         .catch(err => {
             Swal.fire({
