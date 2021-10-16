f9e696baf046f227c4758b0750c312e5af290ee8
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-10-16 23:32:19 +0000

    Election icon and position controller added.

--- public/js/position.js
+++ public/js/position.js
@@ -16,23 +16,19 @@ const displayElections = async() => {
                 let html = `<option value="${ele.id}">${ele.election_name}</option>`;
                 dropdown.insertAdjacentHTML('beforeend', html);
             });
-
-            // document.querySelectorAll(".action-btn").forEach(ele => {
-            //     ele.addEventListener("click", (e) => {
-            //         let id = e.target.id.split("-")[0];
-            //         let classes = e.target.classList;
-            //         let party_name = document.getElementById(`name-${id}`).innerHTML;
-
-            //         classes.forEach(ele => {
-            //             if (ele === 'edit') {
-            //                 displayPartyNameForEditing(id, party_name);
-            //             } else if (ele === 'delete') {
-            //                 deleteParty(id, party_name);
-            //             }
-            //         })
-            //     })
-            // })
         })
 }
+
+const getFormData = () => {
+
+}
+
+
+
+document.querySelector("#create-position").addEventListener("click", (e) => {
+    let position_name = document.querySelector('.position-name');
+    let election_selected = document.getElementById('elections-dropdown');
+    console.log(election_selected.value)
+})
 
 displayElections();
--- routers/position-routes.js
+++ routers/position-routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const positionControllers = require('../controllers/position-controller');
 
 router.get('/position', (req, res) => {
     res.render('position', { page: 'position' });
 })
+
+router.post('/position', positionControllers.createPosition);
+
+router.get('/position', positionControllers.getPositions);
+
+router.patch('/position', positionControllers.updatePosition);
+
+router.delete('/position', positionControllers.deletePosition);
 
 module.exports = router;
