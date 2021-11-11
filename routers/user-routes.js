d146774ea3d0538912b44eb18ad5ad7b15348504
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-11 01:59:59 +0000

    user route and controller added.

--- routers/user-routes.js
+++ routers/user-routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const userControllers = require('../controllers/user-controller');
 
-router.get('/contestant', (req, res) => {
-    res.render('contestant', { page: 'contestant' });
-})
+router.get('/user/create', userControllers.getPositionView);
+
+router.post('/user', userControllers.createPosition);
+
+router.get('/user', userControllers.getPositions);
+
+router.patch('/user', userControllers.updatePosition);
+
+router.delete('/user', userControllers.deletePosition);
 
 module.exports = router;
