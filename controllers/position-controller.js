c4d9d59dbb402f9b93bb7b9261268a0816fdc48d
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-13 22:35:36 +0000

    Insert position implemented.

--- controllers/position-controller.js
+++ controllers/position-controller.js
@@ -1,22 +1,47 @@
-let sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./electionDB.db');
+const POSITIONS = require('../models/table').POSITIONS;
 
 const getPositionView = (req, res) => {
     res.render('position', { page: 'position' });
 }
 
 const createPosition = (req, res) => {
+    req.session.userID = "f759fa54-6640-416a-a01c-9e1ae1b1fd21";
+    req.session.electionID = "8c25132a-5e69-4572-8944-565d5c0eabc6";
+    req.session.role = "superadmin";
+
+    let role = req.session.role;
+    let user_id = req.session.userID;
+    let election_id = req.session.electionID;
     let position_name = req.query.name.trim();
-    let election_id = req.query.id.trim();
+
+    if (role === "superadmin") {
+        election_id = req.query.id.trim();
+    }
 
     if (position_name.length > 0) {
-        db.run(`INSERT INTO positions("position_name", "election_id", "date_created") VALUES(?, ?, datetime('now'));`, position_name, election_id, (err, result) => {
-            if (!err) {
-                res.send({ status: 0, msg: 'position created' });
-            } else {
-                res.send({ status: 1, msg: 'position not created' });
-            }
-        })
+        POSITIONS
+            .findOrCreate({
+                where: {
+                    position_name: position_name,
+                    election_id: election_id
+                },
+                defaults: {
+                    user_id: user_id,
+                    election_id: election_id,
+                    position_name: position_name
+                }
+            })
+            .then(result => {
+                if (result[1] === true) {
+                    res.send({ status: 0, msg: 'Position created' });
+                } else {
+                    res.send({ status: 1, msg: 'Position already exists' });
+                }
+            })
+            .catch(err => {
+                console.log("ERRORRRR>>>>>>>>>>>>>", err);
+                res.send({ status: 1, msg: 'Position not created' });
+            })
     } else {
         res.send({ status: 1, msg: 'Invalid position name' })
     }
@@ -24,14 +49,13 @@ const createPosition = (req, res) => {
 
 const getPositions = (req, res) => {
     if (req.query.id) {
-        let election_id = req.query.id;
-        db.all(`SELECT * FROM positions WHERE election_id = ?`, election_id, (err, data) => {
-            !err ? res.send(data) : res.send(err);
-        });
+        queryPositions(req.query.id, data => {
+            res.send(data);
+        })
     } else {
-        db.all("SELECT * FROM positions;", (err, data) => {
-            !err ? res.send(data) : res.send(err);
-        });
+        queryPositions(null, data => {
+            res.send(data);
+        })
     }
 }
 
@@ -55,6 +79,110 @@ const deletePosition = (req, res) => {
         }
     });
 }
+
+
+// ================================================
+
+
+
+const createParty = (req, res) => {
+    let party_name = req.query.name.trim();
+    req.session.userID = "f759fa54-6640-416a-a01c-9e1ae1b1fd21";
+    req.session.electionID = "8c25132a-5e69-4572-8944-565d5c0eabc6";
+    req.session.role = "superadmin";
+    let userID = req.session.userID;
+    let electionID = req.session.electionID;
+
+    if (party_name.length > 0) {
+
+    } else {
+        res.send({ status: 1, msg: 'Invalid party name' })
+    }
+}
+
+const getParties = async(req, res) => {
+    console.log("Get Parties ====================");
+    if (req.query.id) {
+        queryParties(req.query.id, data => {
+            res.send(data);
+        })
+    } else {
+        queryParties(null, data => {
+            res.send(data);
+        })
+    }
+}
+
+const updateParty = (req, res) => {
+    let party_id = req.query.id;
+    let party_name = req.query.name;
+
+    PARTY.update({
+        party_name: party_name
+    }, {
+        where: {
+            id: party_id
+        }
+    }).then(() => {
+        res.send({ status: 0, msg: 'Party updated' });
+    }).catch(error => {
+        res.send({ status: 1, msg: 'Party not updated' });
+    })
+}
+
+const deleteParty = (req, res) => {
+    let party_id = req.query.id;
+
+    PARTY.destroy({
+        where: {
+            id: party_id
+        }
+    }).then(() => {
+        res.send({ status: 0, msg: 'Party deleted' });
+    }).catch(error => {
+        res.send({ status: 1, msg: 'Party not deleted' });
+    })
+}
+
+const queryPositions = async(position_id, callback) => {
+    if (position_id) {
+        POSITIONS
+            .findOne({
+                where: { id: position_id, status: 'active' }
+            })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    } else {
+        POSITIONS
+            .findAll({
+                where: { status: 'active' },
+                attributes: ['id', 'position_name']
+            })
+            .then(result => {
+                let results = [];
+                result.forEach(ele => {
+                    results.push(ele.dataValues);
+                })
+                return callback(results);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
+
+// queryParties();
+
+// module.exports = {
+//     createParty,
+//     getParties,
+//     updateParty,
+//     deleteParty
+// }
 
 module.exports = {
     getPositionView,
