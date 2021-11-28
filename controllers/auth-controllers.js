cf823dff2f99d2d9040281cbbd5f9a881ed637f8
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-28 08:06:49 +0000

    auth-controller refactored removing commented codes.

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,13 +1,12 @@
 const USERS = require('../models/table').USERS;
 const ROLES = require('../models/table').ROLES;
+
 let res_obj = {
     code: 0,
     msg: "successful"
 }
 
-const loginForm = (req, res) => {
-    res.render('./login', { page: 'login' });
-}
+const loginForm = (req, res) => res.render('./login', { page: 'login' });
 
 const loginDetails = async(req, res) => {
     let res_obj = {
@@ -16,14 +15,11 @@ const loginDetails = async(req, res) => {
     }
 
     getUsers(req.body, result => {
-        console.log("Resut====>>>>", result);
-
         if (result.id) {
             req.session.userID = result.id;
             req.session.electionID = result.election_id;
             req.session.roleID = result.role.dataValues.id;
             req.session.role = result.role.dataValues.role;
-            console.log("==========222", req.session);
         } else {
             res_obj.code = 1;
             res_obj.msg = 'not active user';
@@ -32,31 +28,9 @@ const loginDetails = async(req, res) => {
     })
 }
 
-const getUsers = (data, callback) => {
-    USERS.findAll({
-            include: [ROLES],
-            // attributes: ['id'],
-            where: {
-                email: data.email,
-                password: data.password,
-                status: 'active'
-            }
-        })
-        .then(response => {
-            if (response.length > 0) {
-                return callback(response[0].dataValues);
-            } else {
-                return callback({ code: 1, message: 'user not found' });
-            }
-        })
-        .catch(err => {
-            return callback(err.message);
-        })
-}
+const logOut = (req, res) => req.session.destroy((err) => res.redirect('/login'));
 
-const signUpForm = (req, res) => {
-    res.render('./register', { page: 'register' });
-}
+const signUpForm = (req, res) => res.render('./register', { page: 'register' });
 
 const saveSignUpForm = async(req, res) => {
     await USERS.findOrCreate({
@@ -78,91 +52,31 @@ const saveSignUpForm = async(req, res) => {
     res.send(res_obj);
 }
 
-const logOut = (req, res) => req.session.destroy((err) => res.redirect('/login'));
-
-const getJobs = async(req, res) => {
-    let auth_result = await auth(req.query);
-
-    if (auth_result) {
-        db.all(`SELECT * FROM jobs`, (err, data) => res.send(data))
-    } else {
-        res_obj.code = 401;
-        res_obj.msg = "Sorry! Unauthorized step taken";
-        res.send(res_obj);
-    }
-}
-
-const createJob = async(req, res) => {
-    let title = req.body.title;
-    let job_link = req.body.job_link;
-    let end_date = req.body.end_date;
-
-    await Job
-        .findOrCreate({ where: { title: title, job_link: job_link, end_date: end_date } })
-        .then(suc => {
-            if (!suc[1]) {
-                res_obj.code = 1;
-                res_obj.msg = "Job already exist";
+const getUsers = (data, callback) => {
+    USERS.findAll({
+            include: [ROLES],
+            where: {
+                email: data.email,
+                password: data.password,
+                status: 'active'
+            }
+        })
+        .then(response => {
+            if (response.length > 0) {
+                return callback(response[0].dataValues);
+            } else {
+                return callback({ code: 1, message: 'user not found' });
             }
         })
         .catch(err => {
-            res_obj.code = 1;
-            res_obj.msg = "Job not entered";
-            res_obj.error_msg = err.message;
+            return callback(err.message);
         })
-
-    res.send(res_obj);
-}
-
-const editJob = (req, res) => {
-    let job_id = req.query.id;
-    let title = req.body.title.trim();
-    let job_link = req.body.job_link.trim();
-    let end_date = req.body.end_date.trim();
-
-    if (checkLength(title) > 0 && checkLength(job_link) > 0 && checkLength(end_date) > 0) {
-        db.run(`UPDATE jobs SET title = ?, job_links = ?, end_date = ? WHERE id = ?;`, title, job_link, end_date, job_id, (err) => {
-            if (!err) {
-                res.send({ status: 0, msg: 'Job updated' });
-            } else {
-                res.send({ status: 1, msg: 'Job not updated' });
-            }
-        });
-    } else {
-        db.run(`UPDATE jobs SET title = ? WHERE id = ?;`, title, job_id, (err) => {
-            if (!err) {
-                res.send({ status: 0, msg: 'Job title updated' });
-            } else {
-                res.send({ status: 1, msg: 'Job title not updated' });
-            }
-        });
-    }
-}
-
-const deleteJob = async(req, res) => {
-    let auth_result = await auth(req.query);
-
-    if (auth_result) {
-        let job_id = req.query.id;
-        db.run(`DELETE FROM jobs WHERE id = ?;`, job_id, (err) => {
-            if (!err) {
-                res.send({ status: 0, msg: 'Job deleted' });
-            } else {
-                res.send({ status: 1, msg: 'Job not deleted' });
-            }
-        });
-    } else {
-        res_obj.code = 401;
-        res_obj.msg = "Sorry! Unauthorized step taken";
-    }
-
-    res.send(res_obj);
 }
 
 module.exports = {
     loginForm,
     loginDetails,
+    logOut,
     signUpForm,
-    saveSignUpForm,
-    logOut
+    saveSignUpForm
 }
