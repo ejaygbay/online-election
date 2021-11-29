b7530b27653ec6ba9ddc79fdc09cf218cf029185
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-11-29 07:43:36 +0000

    elections dropdowns display with election only for superadmin.

--- public/js/position.js
+++ public/js/position.js
@@ -1,6 +1,7 @@
 const URL = window.location.origin;
 let position_name = document.querySelector('#new-position-name');
-let election_selected = document.getElementById('elections-dropdown');
+let election_dropdown1 = document.querySelector('#elections-dropdown');
+let election_dropdown2 = document.querySelector("#elections-dropdown2");
 
 
 const displayPositions = (data) => {
@@ -46,115 +47,23 @@ const getElections = () => {
     fetch(`${URL}/election`)
         .then(response => response.json())
         .then(data => {
-            let dropdown = document.querySelector("#elections-dropdown");
-            let dropdown2 = document.querySelector("#elections-dropdown2");
             let html = `<option value="" disabled selected>Select Election</option>`;
 
             data = data.reverse();
 
-            dropdown.innerHTML = "";
-            dropdown2.innerHTML = "";
+            election_dropdown1.innerHTML = "";
+            election_dropdown2.innerHTML = "";
 
-            dropdown.insertAdjacentHTML('beforeend', html);
-            dropdown2.insertAdjacentHTML('beforeend', html);
+            election_dropdown1.insertAdjacentHTML('beforeend', html);
+            election_dropdown2.insertAdjacentHTML('beforeend', html);
 
             data.forEach(ele => {
                 let html = `<option value="${ele.id}">${ele.election_name}</option>`;
-                dropdown.insertAdjacentHTML('beforeend', html);
-                dropdown2.insertAdjacentHTML('beforeend', html);
+                election_dropdown1.insertAdjacentHTML('beforeend', html);
+                election_dropdown2.insertAdjacentHTML('beforeend', html);
             });
         })
 }
-
-/**
- * When the select element for the displaying positions for
- * a specific election is changed, it is handled by this
- */
-document.getElementById('elections-dropdown2').addEventListener("change", () => {
-    let election_id = document.getElementById('elections-dropdown2').value;
-
-    fetch(`${URL}/position?id=${election_id}`)
-        .then(response => response.json())
-        .then(data => {
-            data = data.reverse();
-            displayPositions(data);
-        })
-})
-
-const displayPositionNameForEditing = (id, position_name) => {
-    Swal.fire({
-            title: 'Edit Position',
-            input: 'text',
-            inputValue: position_name,
-            inputAttributes: {
-                autocapitalize: 'off'
-            },
-            showCancelButton: true,
-            confirmButtonText: 'Save',
-            showLoaderOnConfirm: true
-        })
-        .then((result) => {
-            if (result.isConfirmed) {
-                editPosition(id, position_name, result.value);
-            }
-        })
-}
-
-election_selected.addEventListener("change", () => {
-    if (election_selected.value.trim().length > 0 && position_name.value.trim().length > 0) {
-        enableBtn('#create-position-btn');
-    } else {
-        disableBtn('#create-position-btn');
-    }
-})
-
-position_name.addEventListener('keyup', (e) => {
-    if (election_selected.value.trim().length > 0) {
-        validateInputs(position_name.value.trim(), result => {
-            result ? enableBtn('#create-position-btn') : disableBtn('#create-position-btn');
-        })
-    } else {
-        disableBtn('#create-position-btn');
-    }
-
-    // validateInputs(election_selected.value.trim(), result => {
-    //     result ? enableBtn('#create-position-btn') : disableBtn('#create-position-btn');
-    // })
-    // console.log(election_selected.value);
-})
-
-document.querySelector("#create-position-btn").addEventListener("click", (e) => {
-    let data_to_send = {
-        position_name: position_name.value,
-        election_id: election_selected.value
-    }
-
-    makeAPICall(data_to_send, result => {
-        if (result.status === 0) {
-            Swal.fire({
-                icon: 'success',
-                title: `Position name "${position_name.value}" was created`,
-                showConfirmButton: false,
-                timer: 2500
-            })
-            position_name.value = "";
-            election_selected.selectedIndex = 0;
-            disableBtn('#create-position-btn');
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: `Position name "${position_name.value}" already exist`,
-                showConfirmButton: false,
-                timer: 2500
-            })
-        }
-
-        position_name.focus();
-    })
-})
-
-getElections();
-
 
 const editPosition = (id, old_name, new_name) => {
     fetch(`${URL}/position?id=${id}&name=${new_name}`, {
@@ -171,7 +80,6 @@ const editPosition = (id, old_name, new_name) => {
             document.querySelector(`#name-${id}`).innerHTML = new_name;
         })
 }
-
 
 const deletePosition = (id, position_name) => {
     fetch(`${URL}/position?id=${id}`, {
@@ -212,6 +120,98 @@ const validateInputs = async(data, callback) => {
         return callback(false);
 }
 
+const displayPositionNameForEditing = (id, position_name) => {
+    Swal.fire({
+            title: 'Edit Position',
+            input: 'text',
+            inputValue: position_name,
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            showLoaderOnConfirm: true
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+                editPosition(id, position_name, result.value);
+            }
+        })
+}
+
 const enableBtn = (id) => document.querySelector(id).disabled = false;
 
 const disableBtn = (id) => document.querySelector(id).disabled = true;
+
+/**
+ * When the select element for the displaying positions for
+ * a specific election is changed, it is handled by this
+ */
+if (election_dropdown1 && election_dropdown2) {
+    election_dropdown1.addEventListener("change", () => {
+        if (election_dropdown1.value.trim().length > 0 && position_name.value.trim().length > 0) {
+            enableBtn('#create-position-btn');
+        } else {
+            disableBtn('#create-position-btn');
+        }
+    })
+
+    election_dropdown2.addEventListener("change", () => {
+        let election_id = election_dropdown2.value;
+
+        fetch(`${URL}/position?id=${election_id}`)
+            .then(response => response.json())
+            .then(data => {
+                data = data.reverse();
+                displayPositions(data);
+            })
+    })
+
+    getElections();
+}
+
+
+position_name.addEventListener('keyup', (e) => {
+    if (election_dropdown1.value.trim().length > 0) {
+        validateInputs(position_name.value.trim(), result => {
+            result ? enableBtn('#create-position-btn') : disableBtn('#create-position-btn');
+        })
+    } else {
+        disableBtn('#create-position-btn');
+    }
+
+    // validateInputs(election_dropdown1.value.trim(), result => {
+    //     result ? enableBtn('#create-position-btn') : disableBtn('#create-position-btn');
+    // })
+    // console.log(election_dropdown1.value);
+})
+
+document.querySelector("#create-position-btn").addEventListener("click", (e) => {
+    let data_to_send = {
+        position_name: position_name.value,
+        election_id: election_dropdown1.value
+    }
+
+    makeAPICall(data_to_send, result => {
+        if (result.status === 0) {
+            Swal.fire({
+                icon: 'success',
+                title: `Position name "${position_name.value}" was created`,
+                showConfirmButton: false,
+                timer: 2500
+            })
+            position_name.value = "";
+            election_dropdown1.selectedIndex = 0;
+            disableBtn('#create-position-btn');
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: `Position name "${position_name.value}" already exist`,
+                showConfirmButton: false,
+                timer: 2500
+            })
+        }
+
+        position_name.focus();
+    })
+})
