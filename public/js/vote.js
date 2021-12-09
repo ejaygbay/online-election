ce7d4c863cc6344de58f51de2a313656596632ad
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-12-09 11:23:56 +0000

    Converting blob to image 5% done.

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,11 +1,33 @@
-const URL = window.location.origin;
+const URL2 = window.location.origin;
 
 const getContestants = async(election_id) => {
-    // console.log("Election ID:::", URL);
+    // console.log("Election ID:::", URL2);
 
-    fetch(`${URL}/contestant?election_id=${election_id}`)
+    fetch(`${URL2}/contestant?election_id=${election_id}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            // let blob = data[0].photo.data;
+            let blob = "blob:http://localhost:3200/dd9de8b4-b9bc-4e71-8d68-c34e90bc5c52";
+            // const url = window.URL.createObjectURL(blob);
+
+            const reader = new FileReader();
+            reader.addEventListener('loadend', () => {
+                // reader.result contains the contents of blob as a typed array
+                console.log("Done:", reader.result);
+            });
+            reader.readAsArrayBuffer(blob);
+            console.log("Reader>>", reader);
+
+            // let img = new Image()
+            // img.onload = () => {
+            //     URL.revokeObjectURL(url)
+            //     resolve(img)
+            // }
+            // img.src = url
+            console.log(data[0].photo);
+            // console.log("PHOTO::::", blob)
+            document.querySelector("#test-blob").src = "";
+        })
         .catch(err => { console.log("Error+>>>>>>>>>>>>>>", err) })
 }
 
